53169d45d6d09a19fef5ad48144d8573d7069bde
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-10-04 07:27:24 +0300

    fix logo

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,15 +1,17 @@
+'use client'
 import React from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
 import CustomButton from './CustomButton';
+import {  Typography } from '@material-tailwind/react';
 
 
 
 const Nav = () => {
   return (
     <nav className="container mx-auto py-4 flex justify-between items-center mb-10">
-      <Link href="/">
+      <Link href="/" className='relative'>
         <Image
           src="https://marmagroup.ru/local/templates/marma_custom_page/svg/logo.svg"
           alt="logo"
@@ -17,6 +19,8 @@ const Nav = () => {
           height={72}
           className='sm:max-w-[130px]'
         />
+  
+<Typography className='uppercase text-[#393E4F] font-bold text-2xl p-1 bg-white absolute top-0 right-0 sm:text-base'>marma <br/> tools</Typography>
       </Link>
       <Link href="../pages/admin-dashboard">
         <CustomButton>панель администрации</CustomButton>
